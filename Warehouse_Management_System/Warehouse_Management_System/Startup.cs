6e165a182d346d69aee13dc3a48ae945907c9c50
ar640 <ar640915@gmail.com>
2023-01-30 23:31:45 +0530

    Updated

---
+++ Warehouse_Management_System/Warehouse_Management_System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Warehouse_Management_System.Startup))]
+namespace Warehouse_Management_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
